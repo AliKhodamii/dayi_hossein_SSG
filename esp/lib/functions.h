2306373757980a9e92bcfede59cd2cc879c7573b
Sed <s.alikhodami@gmail.com>
2025-04-08 18:41:43 +0330

    developing record insertion and fix bugs

--- esp/lib/functions.h
+++ esp/lib/functions.h
@@ -1,14 +1,16 @@
 #include <Arduino.h>
-#include<ESP8266WiFi.h>
-#include<ArduinoJson.h>
-#include<ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 
-bool wifi_connect(String ssid , String password){
-    WiFi.begin(ssid,password);
+bool wifi_connect(String ssid, String password)
+{
+    WiFi.begin(ssid, password);
     Serial.print("Connecting to ");
     Serial.print(ssid);
-    
-    while(WiFi.status() != WL_CONNECTED){
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
         Serial.print(".");
         delay(500);
     }
@@ -17,76 +19,107 @@ bool wifi_connect(String ssid , String password){
     Serial.print("Connected, IP address: ");
     Serial.println(WiFi.localIP());
 
-    if(WiFi.status() == WL_CONNECTED){
+    if (WiFi.status() == WL_CONNECTED)
+    {
         return true;
     }
-    else{
+    else
+    {
         return false;
     }
 }
 
-String json_encode(JsonDocument data){
+String json_encode(JsonDocument data)
+{
     String result = "";
-    serializeJson(data , result);
+    serializeJson(data, result);
     return result;
 }
 
-JsonDocument json_decode (String json){
+JsonDocument json_decode(String json)
+{
     JsonDocument data;
-    deserializeJson(data , json);
+    deserializeJson(data, json);
     return data;
 }
 
-int httpPost(String url , String destination , String data){
-    data = destination+"="+data;
+int httpPost(String url, String destination, String data)
+{
+    data = destination + "=" + data;
 
     WiFiClient client;
     HTTPClient http;
-    http.begin(client , url);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    int httpResCode = http.POST(data);
-    Serial.print("Http post response code: ");
-    Serial.println(httpResCode);
+    int httpResCode = -1;
+    while (httpResCode != 200)
+    {
+        http.begin(client, url);
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpResCode = http.POST(data);
+        Serial.print("Http post response code: ");
+        Serial.print(httpResCode);
+        if (httpResCode != 200)
+        {
+            Serial.println(" ...Failed.");
+        }
+    }
+
+    Serial.println(" ...Succeed.");
     return httpResCode;
 }
 
-String httpGet(String url , String filename) {
-  WiFiClient client;
-  HTTPClient http;
-  url = url+"?file="+filename;
-  http.begin(client, url);
-  int httpResponseCode = http.GET();
-  String payload = "{}"; 
-  if (httpResponseCode>0) {
-    Serial.print("Http get response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  http.end();
-  return payload;
+String httpGet(String url, String filename)
+{
+    WiFiClient client;
+    HTTPClient http;
+    int httpResponseCode = -1;
+
+    url = url + "?file=" + filename;
+    String payload = "{}";
+
+    while (httpResponseCode != 200)
+    {
+        http.begin(client, url);
+        httpResponseCode = http.GET();
+
+        if (httpResponseCode > 0)
+        {
+            Serial.print("Http get response code:  ");
+            Serial.print(httpResponseCode);
+            Serial.println(" ...Succeed.");
+            payload = http.getString();
+        }
+        else
+        {
+            Serial.print("Http get response code:  ");
+            Serial.print(httpResponseCode);
+            Serial.println(" ...Failed.");
+        }
+    }
+
+    http.end();
+    return payload;
 }
 
-int humidity_read(){
+int humidity_read()
+{
     int humidity = 0;
-    for(int i=0 ; i<10 ; i++){
+    for (int i = 0; i < 10; i++)
+    {
         humidity += analogRead(A0);
     }
-    humidity = humidity/10;
+    humidity = humidity / 10;
     return humidity;
 }
 
-String time(){
+String time()
+{
     int mil = millis();
-    int d = mil/86400000;
-    mil -= (d*86400000);
-    int h = mil/3600000;
-    mil -=h*3600000;
-    int m = mil/60000;
-    mil -= m*60000;
+    int d = mil / 86400000;
+    mil -= (d * 86400000);
+    int h = mil / 3600000;
+    mil -= h * 3600000;
+    int m = mil / 60000;
+    mil -= m * 60000;
     int s = mil / 1000;
 
     String time = String(d) + " : " + String(h) + " : " + String(m) + " : " + String(s);
--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,7 +1,7 @@
-#include<Arduino.h>
-#include<ESP8266WiFi.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include<ArduinoJson.h>
+#include <ArduinoJson.h>
 #include "../lib/functions.h"
 
 // pins
@@ -11,7 +11,7 @@ int valveP = D7;
 bool valve = true;
 bool copy = false;
 int humidity = 1000;
-int duration = 0;
+long unsigned int duration = 0;
 
 // cmd variables
 String valveCmd = "";
@@ -21,43 +21,46 @@ String postUrl = "http://sed-smarthome.ir/dayi_hossein/server/postInfo.php";
 String getUrl = "http://sed-smarthome.ir/dayi_hossein/server/getInfoWeb.php";
 String DbUrl = "http://sed-smarthome.ir/dayi_hossein/server/insertIrrRec.php";
 
-
-JsonDocument sysInfo,cmd;
+JsonDocument sysInfo, cmd;
 
 // jsons
 String sysInfoJson;
 String cmdJson;
 
 // time
-long long int every5s = 0;
-int valveTimer = 0;
-int copyTimer = 0;
+long unsigned int every5s = 0;
+long unsigned int valveTimer = 0;
+long unsigned int copyTimer = 0;
 
 // request codes
 int postResCode = 0;
 
-void setup(){
+void setup()
+{
+  pinMode(valveP, OUTPUT);
+  digitalWrite(valveP, LOW);
+
   Serial.begin(9600);
   Serial.println("");
   Serial.println("");
   Serial.println("");
   Serial.println("");
 
-  String ssid = "TP_Sed"; 
+  String ssid = "TP_Sed";
   String password = "87654321";
 
-  wifi_connect(ssid,password);
+  wifi_connect(ssid, password);
 
-  //set initial values
+  // set initial values
   valve = false;
   humidity = humidity_read();
-  
+
   // get duration from sysInfo on host
-  sysInfoJson = httpGet(getUrl,"sys");
+  sysInfoJson = httpGet(getUrl, "sys");
   sysInfo = json_decode(sysInfoJson);
   duration = sysInfo["duration"];
 
-  //prepare data for sysInfo post
+  // prepare data for sysInfo post
   sysInfo["time"] = time();
   sysInfo["valve"] = valve;
   sysInfo["humidity"] = humidity;
@@ -65,25 +68,27 @@ void setup(){
   sysInfo["copy"] = copy;
   sysInfoJson = json_encode(sysInfo);
 
-  //post request to sysInfo to set new status 
+  // post request to sysInfo to set new status
   Serial.println("Posting init sysInfo");
-  postResCode = httpPost(postUrl,"sysInfo",sysInfoJson);
-  
-  //prepare data for cmd post
+  postResCode = httpPost(postUrl, "sysInfo", sysInfoJson);
+
+  // prepare data for cmd post
   cmd["valveCmd"] = "noAction";
   cmd["restartCmd"] = false;
   cmd["espConfirm"] = false;
   cmd["durationCmd"] = duration;
   cmdJson = json_encode(cmd);
 
-  //post request to cmd to set now command there
+  // post request to cmd to set now command there
   Serial.println("Posting init cmdInfo");
-  postResCode = httpPost(postUrl,"cmdInfo",cmdJson);
+  postResCode = httpPost(postUrl, "cmdInfo", cmdJson);
 }
 
-void loop(){
+void loop()
+{
   // loop every 5 sec
-  if(millis() - every5s >= 5000){
+  if (millis() - every5s >= 5000)
+  {
     Serial.println("");
     Serial.println("");
     Serial.print("--------");
@@ -91,50 +96,58 @@ void loop(){
     Serial.println("--------");
     Serial.println("");
 
-
     // get cmd file
-    Serial.println("Getting cmd");
-    cmdJson = httpGet(getUrl,"cmd");
+    Serial.println("Getting cmd...");
+    cmdJson = httpGet(getUrl, "cmd");
     cmd = json_decode(cmdJson);
     valveCmd = String(cmd["valveCmd"]);
 
     // check if open command is sent
-    if(String(cmd["valveCmd"])== "open"){
+    if (String(cmd["valveCmd"]) == "open")
+    {
       Serial.println("Open command received \n Opening valve");
 
       // open valve
       valve = true;
       copy = true;
       duration = cmd["durationCmd"];
-      Serial.println("du:"+String(duration));
 
       // set time variables
       valveTimer = millis();
       copyTimer = millis();
 
       // post new status
-      Serial.println("Posting new status");
+      Serial.println("Posting new status...");
       sysInfo["time"] = time();
       sysInfo["valve"] = valve;
       sysInfo["copy"] = copy;
       sysInfo["duration"] = duration;
       sysInfoJson = json_encode(sysInfo);
-      httpPost(postUrl,"sysInfo",sysInfoJson);
+      httpPost(postUrl, "sysInfo", sysInfoJson);
 
       // post confirm and no action in cmd
-      Serial.println("Posting new cmd");
+      Serial.println("Posting new cmd...");
       cmd["valveCmd"] = "noAction";
       cmd["confirm"] = true;
       cmdJson = json_encode(cmd);
-      httpPost(postUrl,"cmdInfo",cmdJson);
+      httpPost(postUrl, "cmdInfo", cmdJson);
 
       // insert new irr record to database
-      httpPost(DbUrl,"insertIrrRec",sysInfoJson);
+      Serial.println("Inserting new data to db...");
+      if (httpPost(DbUrl, "insertIrrRec", sysInfoJson))
+      {
+        Serial.println("Succeed.");
+      }
+      else
+      {
+        Serial.println("failed.");
+      }
     }
 
     // check if close command is sent
-    if(String(cmd["valveCmd"]) == "close"){
-       Serial.println("Close command received \n Closing valve");
+    if (String(cmd["valveCmd"]) == "close")
+    {
+      Serial.println("Close command received \n Closing valve");
 
       // open valve
       valve = false;
@@ -142,45 +155,51 @@ void loop(){
       duration = cmd["durationCmd"];
 
       // post new status
-      Serial.println("Posting new status");
+      Serial.println("Posting new status...");
       sysInfo["time"] = time();
       sysInfo["valve"] = valve;
       sysInfo["copy"] = copy;
       sysInfo["duration"] = duration;
       sysInfoJson = json_encode(sysInfo);
-      httpPost(postUrl,"sysInfo",sysInfoJson);
+      httpPost(postUrl, "sysInfo", sysInfoJson);
 
       // post confirm and no action in cmd
-      Serial.println("Posting new cmd");
+      Serial.println("Posting new cmd...");
       cmd["valveCmd"] = "noAction";
       cmd["confirm"] = true;
       cmdJson = json_encode(cmd);
-      httpPost(postUrl,"cmdInfo",cmdJson);
+      httpPost(postUrl, "cmdInfo", cmdJson);
     }
 
     // valve status
-    valve ? digitalWrite(valveP,HIGH) : digitalWrite(valveP,LOW);
+    if (valve)
+    {
+      digitalWrite(valveP, HIGH);
+    }
+    else
+    {
+      digitalWrite(valveP, LOW);
+    }
 
     // check if it's time to close valve
-    if(valve && (millis() - valveTimer) > duration * 60000)
+    if (valve && (millis() - valveTimer) > duration * 60000)
     {
       Serial.println("Duration is deon\n Valve is closing");
       valve = false;
-      digitalWrite(valveP,LOW);
+      digitalWrite(valveP, LOW);
 
       // post new status
-      Serial.println("Posting new status");
+      Serial.println("Posting new status...");
       sysInfo["time"] = time();
       sysInfo["valve"] = valve;
       sysInfo["copy"] = copy;
       sysInfo["duration"] = duration;
       sysInfoJson = json_encode(sysInfo);
-      httpPost(postUrl,"sysInfo",sysInfoJson);
-
+      httpPost(postUrl, "sysInfo", sysInfoJson);
     }
     // post status
     Serial.println("Posting status");
-  
+
     humidity = humidity_read();
     sysInfo["time"] = time();
     sysInfo["valve"] = valve;
@@ -189,8 +208,7 @@ void loop(){
     sysInfo["humidity"] = humidity;
 
     sysInfoJson = json_encode(sysInfo);
-    httpPost(postUrl,"sysInfo",sysInfoJson);
-    
+    httpPost(postUrl, "sysInfo", sysInfoJson);
 
     Serial.println("sysInfoJson:");
     Serial.println(sysInfoJson);
@@ -199,7 +217,8 @@ void loop(){
   }
 
   // check if copy time is done
-  if (millis() - copyTimer > 10000){
+  if (millis() - copyTimer > 10000)
+  {
     copy = false;
   }
 }
